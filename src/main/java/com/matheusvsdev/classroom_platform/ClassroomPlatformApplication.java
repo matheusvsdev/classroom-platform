bf72425476b533ef5c60104163b17d0279076677
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-11-24 17:31:29 -0300

    first commit

---
+++ src/main/java/com/matheusvsdev/classroom_platform/ClassroomPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.matheusvsdev.classroom_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusvsdev/classroom_platform/ClassroomPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusvsdev.classroom_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
