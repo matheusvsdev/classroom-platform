131a82a429995559ecb5c90cec7aaa72b0c8bada
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-11-24 19:35:49 -0300

    CourseEntity implementation

---
+++ src/main/java/com/matheusvsdev/classroom_platform/entity/CourseEntity.java
@@ -0,0 +1,76 @@
+package com.matheusvsdev.classroom_platform.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_course")
+public class CourseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imgUri;
+
+    private String imgGrayUri;
+
+    public CourseEntity() {
+    }
+
+    public CourseEntity(Long id, String name, String imgUri, String imgGrayUri) {
+        this.id = id;
+        this.name = name;
+        this.imgUri = imgUri;
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public String getImgGrayUri() {
+        return imgGrayUri;
+    }
+
+    public void setImgGrayUri(String imgGrayUri) {
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        CourseEntity that = (CourseEntity) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
