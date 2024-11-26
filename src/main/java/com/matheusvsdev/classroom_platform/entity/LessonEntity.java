136b6f0e617760e0ed02ae58127df0d45da92a7a
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-11-26 14:51:02 -0300

    ORM, LessonEntity

--- src/main/java/com/matheusvsdev/classroom_platform/entity/LessonEntity.java
+++ src/main/java/com/matheusvsdev/classroom_platform/entity/LessonEntity.java
@@ -2,7 +2,7 @@ package com.matheusvsdev.classroom_platform.entity;
 
 import jakarta.persistence.*;
 
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_lesson")
@@ -12,18 +12,34 @@ public abstract class LessonEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
-
     private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private SectionEntity section;
+
+    @OneToMany(mappedBy = "lesson")
+    private List<DeliverEntity> deliveries = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done",
+            joinColumns = @JoinColumn(name = "lesson_id"),
+            inverseJoinColumns = {
+                            @JoinColumn(name = "user_id"),
+                            @JoinColumn(name = "offer_id")
+            }
+    )
+    private Set<EnrollmentEntity> enrollmentsDone = new HashSet<>();
 
     public LessonEntity() {
     }
 
-    public LessonEntity(Long id, String title, Integer position) {
+    public LessonEntity(Long id, String title, Integer position, SectionEntity section) {
         this.id = id;
         this.title = title;
         this.position = position;
+        this.section = section;
     }
 
     public Long getId() {
@@ -48,6 +64,22 @@ public abstract class LessonEntity {
 
     public void setPosition(Integer position) {
         this.position = position;
+    }
+
+    public SectionEntity getSection() {
+        return section;
+    }
+
+    public void setSection(SectionEntity section) {
+        this.section = section;
+    }
+
+    public List<DeliverEntity> getDeliveries() {
+        return deliveries;
+    }
+
+    public Set<EnrollmentEntity> getEnrollmentsDone() {
+        return enrollmentsDone;
     }
 
     @Override
