a3e7a27a4936fb312a29c3e993bc2271fafe25cf
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-11-26 15:08:23 -0300

    ORM, ReplyEntity

--- src/main/java/com/matheusvsdev/classroom_platform/entity/ReplyEntity.java
+++ src/main/java/com/matheusvsdev/classroom_platform/entity/ReplyEntity.java
@@ -3,7 +3,9 @@ package com.matheusvsdev.classroom_platform.entity;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_reply")
@@ -13,9 +15,26 @@ public class ReplyEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition="TEXT")
     private String body;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private TopicEntity topic;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private UserEntity author;
+
+    @ManyToMany
+    @JoinTable(name = "tb_reply_likes",
+            joinColumns = @JoinColumn(name = "reply_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<UserEntity> likes = new HashSet<>();
 
     public ReplyEntity() {
     }
@@ -42,6 +61,26 @@ public class ReplyEntity {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public TopicEntity getTopic() {
+        return topic;
+    }
+
+    public void setTopic(TopicEntity topic) {
+        this.topic = topic;
+    }
+
+    public UserEntity getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(UserEntity author) {
+        this.author = author;
+    }
+
+    public Set<UserEntity> getLikes() {
+        return likes;
     }
 
     @Override
